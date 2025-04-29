815899160de1c74bb63bc00313425e8f22ea654c
Prince chukwuemeka <162118418+pchukwuemeka424@users.noreply.github.com>
2025-04-29 22:01:23 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
-const nextConfig = {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     domains: ['wfhjcblhlsdtxpwuxvgm.supabase.co', 'images.unsplash.com'],
   },
@@ -12,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
