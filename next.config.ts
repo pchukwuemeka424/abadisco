868b6806a99ae500e132a3ff98863265a03aa651
Prince chukwuemeka <162118418+pchukwuemeka424@users.noreply.github.com>
2025-04-29 21:53:53 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ const nextConfig = {
   images: {
     domains: ['wfhjcblhlsdtxpwuxvgm.supabase.co', 'images.unsplash.com'],
   },
+  // Ignore TypeScript errors during the build process
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
